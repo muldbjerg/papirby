383730fa178d02bb907c35338ca32ca0b24fbc37
Steffen Muldbjerg <steffenmuldbjerg@gmail.com>
2024-08-22 22:17:43 +0200

    Update header.php

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
 	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri()?>/favicon/apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_stylesheet_directory_uri()?>/favicon/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_stylesheet_directory_uri()?>/favicon/favicon-16x16.png">
-	<link rel="manifest" href="<?php echo bloginfo('template_directory'); ?>/favicon/site.webmanifest">
+	<link rel="manifest" href="./favicon/site.webmanifest">
 	<link rel="mask-icon" href="<?php echo get_stylesheet_directory_uri()?>/favicon/safari-pinned-tab.svg" color="#23660f">
 	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri()?>/favicon/favicon.ico">
 	<meta name="msapplication-TileColor" content="#23660f">
