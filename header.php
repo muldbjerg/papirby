bc54d535acbdcd4519ed75ef9e1df0f2f1085df6
Steffen Muldbjerg <steffenmuldbjerg@gmail.com>
2024-08-22 22:19:18 +0200

    Update header.php

--- header.php
+++ header.php
@@ -11,14 +11,14 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri()?>/favicon/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_stylesheet_directory_uri()?>/favicon/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_stylesheet_directory_uri()?>/favicon/favicon-16x16.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/wp-content/themes/papirby/favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/wp-content/themes/papirby/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/wp-content/themes/papirby/favicon/favicon-16x16.png">
 	<link rel="manifest" href="/wp-content/themes/papirby/favicon/site.webmanifest">
-	<link rel="mask-icon" href="<?php echo get_stylesheet_directory_uri()?>/favicon/safari-pinned-tab.svg" color="#23660f">
-	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri()?>/favicon/favicon.ico">
+	<link rel="mask-icon" href="/wp-content/themes/papirby/favicon/safari-pinned-tab.svg" color="#23660f">
+	<link rel="shortcut icon" href="/wp-content/themes/papirby/favicon/favicon.ico">
 	<meta name="msapplication-TileColor" content="#23660f">
-	<meta name="msapplication-config" content="<?php echo get_stylesheet_directory_uri()?>/favicon/browserconfig.xml">
+	<meta name="msapplication-config" content="/wp-content/themes/papirby/favicon/browserconfig.xml">
 	<meta name="theme-color" content="#23660f">
 
 	<?php wp_head(); ?>
